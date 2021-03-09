bd7a8e06d2d2a4a1fec965d01afa70007997445c
Nathan <njeberhardt@cpp.edu>
2021-03-08 21:45:39 -0800

    added risk

--- src/pages/Graphs.js
+++ src/pages/Graphs.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import {inputButtons} from '../components/inputButtons'
+import {Button} from '../components/Button.js'
 
 function Graphs() {
+	//const [button, setButton] = useState(true);
+
+	//const handleClick = () => setClick(!click);
 	return (
-		<div className='Graphs'>
-			<h1>Graphs</h1>
+		<div className='home'>
+			<div className='item'> 
+			<h1>Please Enter your information</h1>
+				{inputButtons.map((item) => {
+				return (
+					<>
+					{item.title}
+					<br/>
+					<input type={item.type} width={item.width} className={item.cName} placeholder={item.place} />
+					<br/>
+					</>
+					);
+				})}
+				<Button buttonStyle='btn--outline'>Go</Button>
+			</div>
+
 		</div>
 	)
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,23 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import c from '../images/c.jpg'
 
 function Home() {
 	return (
+	<>
 		<div className='home'>
-			<h1>Please Enter your information</h1>
-			<div> Enter your age...
-				<br/>
-				<input type="text" placeholder="Type age here..." />
-				<button>Add</button>
+			<div className='item'>
+			<h1>Corona Virus - How much at risk are you?</h1>
+			<img src= {c} width="500" height="300" />
+			<br/>
+			<Link to="/test"> Take our Test</Link>
 			</div>
+
 		</div>
+		<div className='dashboard'>
+
+		</div>
+	</>
 	)
 }
 
